7553e7f83642589ee094b8b1e36a13ff288e9219
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2019-03-08 17:59:55 +0000

    Updates

--- osx/bin/changeinstallnames.sh
+++ osx/bin/changeinstallnames.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-#install_name_tool -change @executable_path/../Frameworks/Ogre.framework/Versions/1.10.0/Ogre @loader_path/../Ogre.framework/Versions/1.10.0/Ogre OgreOverlay.framework/OgreOverlay
-#install_name_tool -change @executable_path/../Frameworks/OgreOverlay.framework/Versions/1.10.0/OgreOverlay @loader_path/../OgreOverlay.framework/Versions/1.10.0/OgreOverlay OgreOverlay.framework/OgreOverlay
 
-for i in *.framework/;do
+for i in *.framework/
+do
     lib=$i${i//.*}
     
     otool -L $lib|grep \@executable_path|while read i
@@ -11,4 +10,6 @@ for i in *.framework/;do
         i=${i// \(*}
         install_name_tool -change $i \@loader_path/..${i#\@executable_path/../Frameworks} $lib
     done
+    
+    otool -L $lib
 done
