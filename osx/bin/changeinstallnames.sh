50dea1024491963a56858723c132c52c69561dc0
Eric Kerfoot <eric.kerfoot@kcl.ac.uk>
2019-03-08 18:26:30 +0000

    Updates

--- osx/bin/changeinstallnames.sh
+++ osx/bin/changeinstallnames.sh
@@ -4,6 +4,7 @@
 for i in *.framework/
 do
     lib=$i${i//.*}
+    echo $lib
     
     otool -L $lib|grep \@executable_path|while read i
     do
@@ -11,6 +12,6 @@ do
         #install_name_tool -change $i \@loader_path/..${i#\@executable_path/../Frameworks} $lib
     done
     
-    install_name_tool -change @loader_path/../Ogre.framework/Versions/1.10.0/Ogre @loader_path/../Ogre.framework/Ogre $lib
+    #install_name_tool -change @loader_path/../Ogre.framework/Versions/1.10.0/Ogre @loader_path/../Ogre.framework/Ogre $lib
     otool -L $lib
 done
